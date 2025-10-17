47792fe1622ba2ab43a11cc2f9558ee331e2a9c1
eliastrk <eliasturki003@gmail.com>
2025-10-17 12:43:19 +0200

    TP1

---
+++ src/main/java/FoodFastUtils.java
@@ -0,0 +1,30 @@
+public class FoodFastUtils {
+
+    public static String deliveryPlanner(int n){
+        if (n%3 == 0 && n%5 == 0){ return "FizzBuzz" ;}
+        if (n%3 == 0){ return "Fizz" ;}
+        if (n%5 == 0){ return "Buzz" ;}
+        return "";
+    }
+
+    static boolean isLeapYear(int year){
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+    }
+
+    public static int sumUpTo(int n){
+        int sum = 0;
+        for (int i = 1; i <= n; i++){
+            sum += i;
+        }
+        return sum;
+    }
+
+    public static String anonymize(String text){
+        String str = "";
+
+        for (int i = 0; i < text.length(); i++){
+            str = text.charAt(i) + str;
+        }
+        return str;
+    }
+}
---
+++ src/test/java/FoodFastUtilsTest.java
@@ -0,0 +1,51 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FoodFastUtilsTest {
+
+    @Test
+    public void testDeliveryPlanner(){
+        int n1 = 30;
+        int n2 = 9;
+        int n3 = 10;
+        int n4 = 8;
+
+        Assertions.assertEquals("FizzBuzz", FoodFastUtils.deliveryPlanner(n1));
+        Assertions.assertEquals("Fizz", FoodFastUtils.deliveryPlanner(n2));
+        Assertions.assertEquals("Buzz", FoodFastUtils.deliveryPlanner(n3));
+        Assertions.assertEquals("", FoodFastUtils.deliveryPlanner(n4));
+    }
+
+    @Test
+    public void testIsLeapYear(){
+        int y1 = 2004;
+        int y2 = 2400;
+        int y3 = 2100;
+        int y4 = 2003;
+
+        Assertions.assertTrue(FoodFastUtils.isLeapYear(y1));
+        Assertions.assertTrue(FoodFastUtils.isLeapYear(y2));
+        Assertions.assertFalse(FoodFastUtils.isLeapYear(y3));
+        Assertions.assertFalse(FoodFastUtils.isLeapYear(y4));
+    }
+
+    @Test
+    public void testSumUpTo(){
+        int n1 = 0;
+        int n2 = 5;
+
+        Assertions.assertEquals(0, FoodFastUtils.sumUpTo(n1));
+        Assertions.assertEquals(15, FoodFastUtils.sumUpTo(n2));
+    }
+
+    @Test
+    public void anonymize(){
+        String str1 = "";
+        String str2 = "KayaK";
+        String str3 = "Salut";
+
+        Assertions.assertEquals("", FoodFastUtils.anonymize(str1));
+        Assertions.assertEquals("KayaK", FoodFastUtils.anonymize(str2));
+        Assertions.assertEquals("tulaS", FoodFastUtils.anonymize(str3));
+    }
+}
